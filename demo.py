1cd8844385e48cb73c5becad32067e719ae8663c
alexmendes89 <167451349+mendesalex89@users.noreply.github.com>
2024-09-15 16:14:38 +0100

    demo.py

--- demo.py
+++ demo.py
@@ -1,4 +1,11 @@
-from us_visa.pipline.training_pipeline import TrainPipeline
 
-obj = TrainPipeline()
-obj.run_pipeline()
+from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
+
+# logging.info("Welcome to our custom log")
+
+try:
+    a = 2/0
+except Exception as e:
+    raise USvisaException(e, sys)
